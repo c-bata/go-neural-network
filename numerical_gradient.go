4ffe982760c9b0cab6ce4c86295868e87a096aca
c-bata <contact@c-bata.link>
2017-02-09 18:33:59 +0900

    Implement numerical gradient

---
+++ numerical_gradient.go
@@ -0,0 +1,25 @@
+package neural_network
+
+type funcType func([]float64) float64
+
+
+func NumericalGradient(f funcType, x [][]float64) [][]float64 {
+	var h float64 = 0.0001
+	grad := make([][]float64, len(x))
+
+	for i, a := range x {
+		grad[i] = make([]float64, len(a))
+
+		for j, b := range a {
+			a[j] = b + h
+			var fxh1 float64 = f(a)
+
+			a[j] = b - h
+			var fxh2 float64 = f(a)
+			grad[i][j] = (fxh1 - fxh2) / (2 * h)
+
+			a[j] = b
+		}
+	}
+	return grad
+}
---
+++ numerical_gradient_test.go
@@ -0,0 +1,40 @@
+package neural_network
+
+import (
+	"testing"
+	"math"
+)
+
+
+func SumSquared(x []float64) float64 {
+	var sum float64 = 0
+	for _, a := range x {
+		sum += math.Pow(a, 2)
+	}
+	return sum
+}
+
+
+func TestNumericalGradient(t *testing.T) {
+	const (
+		NearEqualThreshold = 0.00000001
+	)
+
+	x := [][]float64{{3.0}, {4.0}}
+	a := NumericalGradient(SumSquared, x)
+	if math.Abs(a[0][0] - 6.0) > NearEqualThreshold || math.Abs(a[1][0] - 8.0) > NearEqualThreshold {
+		t.Fatal("NumericalGradient([][]float64{{3.0}, {4.0}}) should be []float64{{6.0}, {8.0}}, but doesn't match.")
+	}
+
+	x = [][]float64{{0.0}, {2.0}}
+	a = NumericalGradient(SumSquared, x)
+	if math.Abs(a[0][0] - 0.0) > NearEqualThreshold || math.Abs(a[1][0] - 4.0) > NearEqualThreshold {
+		t.Fatal("NumericalGradient([][]float64{{0.0}, {2.0}}) should be []float64{{0.0}, {4.0}}, but doesn't match.")
+	}
+
+	x = [][]float64{{3.0}, {0.0}}
+	a = NumericalGradient(SumSquared, x)
+	if math.Abs(a[0][0] - 6.0) > NearEqualThreshold || math.Abs(a[1][0] - 0.0) > NearEqualThreshold {
+		t.Fatal("NumericalGradient([][]float64{{3.0}, {0.0}}) should be []float64{{6.0}, {0.0}}, but doesn't match.")
+	}
+}
