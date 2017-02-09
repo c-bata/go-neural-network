a9d377ffbedbde73476cea92d046a778f5b2623d
c-bata <contact@c-bata.link>
2017-02-09 22:27:26 +0900

    Add mean squared error function

---
+++ functions/mean_squared_error.go
@@ -0,0 +1,12 @@
+package functions
+
+import "math"
+
+func MeanSquaredError(x []float64, t []float64) float64 {
+	var sum float64 = 0
+
+	for i := 0; i < len(x); i++ {
+		sum += math.Pow(x[i] - t[i], 2)
+	}
+	return sum / 2
+}
---
+++ functions/mean_squared_error_test.go
@@ -0,0 +1,22 @@
+package functions
+
+import "testing"
+
+func TestMeanSquaredError(t *testing.T) {
+	const (
+		NearEqualThreshold = 1e-8
+	)
+
+	tk := []float64{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}
+
+	xk := []float64{0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0}
+	if MeanSquaredError(xk, tk) - 0.09750000000 > NearEqualThreshold {
+		t.Fatal("MeanSquaredError() should be 0.09750000000, but doesn't match.")
+	}
+
+
+	xk = []float64{0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0}
+	if MeanSquaredError(xk, tk) - 0.59750000000 > NearEqualThreshold {
+		t.Fatal("MeanSquaredError() should be 0.59750000000, but doesn't match.")
+	}
+}
