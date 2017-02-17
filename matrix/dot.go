99d9cb46530a885165c6021cba28d737ddee9a3e
c-bata <contact@c-bata.link>
2017-02-17 12:59:48 +0900

    Add several dot calculation

--- matrix/dot.go
+++ matrix/dot.go
@@ -1,6 +1,29 @@
 package matrix
 
-func Dot(a [][]float64, b [][]float64) [][]float64 {
+func Dot1x1(a []float64, b []float64) float64 {
+	var sum float64 = 0
+	for i := 0; i < len(a); i++ {
+		sum += a[i] * b[i]
+	}
+	return sum
+}
+
+func Dot1x2(a []float64, b [][]float64) []float64 {
+	w := len(a)
+	h := len(b[0])
+	c := make([]float64, h)
+
+	for i := 0; i < h; i++ {
+		var sum float64 = 0
+		for j := 0; j < w; j++ {
+			sum += a[j] * b[j][i]
+		}
+		c[i] = sum
+	}
+	return c
+}
+
+func Dot2x2(a [][]float64, b [][]float64) [][]float64 {
 	w := len(a)
 	h := len(b[0])
 	c := make([][]float64, h)
--- matrix/dot_test.go
+++ matrix/dot_test.go
@@ -1,17 +1,40 @@
 package matrix
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+)
 
-func TestDot(t *testing.T) {
+func TestDot1x1(t *testing.T) {
+	a := []float64{1, 2, 3}
+	b := []float64{1, 2, 3}
+	var c float64 = Dot1x1(a, b)
+
+	if c != 1*1+2*2+3*3 {
+		t.Errorf("Dot1x1() want %#v got %#v.", 1*1+2*2+3*3, c)
+	}
+}
+
+func TestDot1x2(t *testing.T) {
+	a := []float64{1, 2, 3}
+	b := [][]float64{{1, 4}, {2, 5}, {3, 6}}
+	var c []float64 = Dot1x2(a, b)
+
+	if !reflect.DeepEqual(c, []float64{14, 32}) {
+		t.Errorf("Dot1x2() want %#v got %#v.", []float64{14, 32}, c)
+	}
+}
+
+func TestDot2x2(t *testing.T) {
 	a := [][]float64{{1, 2, 3}, {4, 5, 6}}
 	b := [][]float64{{1, 2}, {3, 4}, {5, 6}}
-	c := Dot(a, b)
+	c := Dot2x2(a, b)
 
 	if len(c) != 2 || len(c[0]) != 2 {
-		t.Fatal("Dot() should return 2x2 arrays.")
+		t.Fatal("Dot2x2() should return 2x2 arrays.")
 	}
 
-	if c[0][0] != 1*1+2*3+3*5 {
-		t.Fatal("c[0][0] should be 22(= 1*1 + 2*3 + 3*5).")
+	if !reflect.DeepEqual(c, [][]float64{{22, 28}, {49, 64}}) {
+		t.Errorf("Dot2x2() want %#v got %#v.", [][]float64{{22, 28}, {49, 64}}, c)
 	}
 }
